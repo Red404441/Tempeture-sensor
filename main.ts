ee097581389d045e96301883b331a317384f07a6
Red404441 <socallaghan24@hcetss.ie>
2025-02-11 13:46:03 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+basic.forever(function () {
+    if (input.temperature() < 15) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (input.temperature() < 30) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else if (input.temperature() < 50) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
